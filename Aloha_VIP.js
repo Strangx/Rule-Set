0fa9ac9af34ef363721910cb6530cb4ca10cd120
Mr Strange <77616081+Strangx@users.noreply.github.com>
2021-05-31 18:29:09 +0630

    Create Aloha_VIP.js

---
+++ Aloha_VIP.js
@@ -0,0 +1,9 @@
+^https:\/\/api\.alohaprofile\.com\/v1\/profile_info\/.* url script-response-body
+
+
+*/var obj = JSON.parse($response.body); 
+obj['is_premium'] = true;
+obj['_end_of_premium'] = 2022-06-07 06:57:57;
+obj['end_of_premium'] = 1653997940611;
+obj['error'] = null;
+$done({body: JSON.stringify(obj)});
