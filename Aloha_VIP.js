9b88ec5e8bdce960030cc117338b44cc947df08e
Mr Strange <77616081+Strangx@users.noreply.github.com>
2021-05-31 18:31:05 +0630

    Update Aloha_VIP.js

--- Aloha_VIP.js
+++ Aloha_VIP.js
@@ -1,6 +1,5 @@
 ^https:\/\/api\.alohaprofile\.com\/v1\/profile_info\/.* url script-response-body
-
-
+https://raw.githubusercontent.com/Strangx/Rule-Set/main/Aloha_VIP.js
 */var obj = JSON.parse($response.body); 
 obj['is_premium'] = true;
 obj['_end_of_premium'] = 2022-06-07 06:57:57;
