5344c0f2a8bb63ce4bf1e96c5b6363d0418738f2
Mr Strange <77616081+Strangx@users.noreply.github.com>
2021-06-02 02:58:15 +0630

    Update Aloha_VIP.js

--- Aloha_VIP.js
+++ Aloha_VIP.js
@@ -2,9 +2,15 @@
 ^https:\/\/api\.alohaprofile\.com\/v1\/profile_info\/.* url script-response-body
 https://raw.githubusercontent.com/Strangx/Rule-Set/main/Aloha_VIP.js
 */
- var obj = JSON.parse($response.body); 
- obj['is_premium'] = true;
- obj['_end_of_premium'] = 2022-06-07 06:57:57;
- obj['end_of_premium'] = 1653997940611;
- obj['error'] = null;
- $done({body: JSON.stringify(obj)});
+let obj = JSON.parse($response.body);
+obj = {
+    "is_premium" : true,
+    "id" : 1003460,
+    "email_verified" : true,
+    "_end_of_premium" : "2021-06-07 06:57:57",
+    "end_of_premium" : 1623049077,
+    "email" : "mr.strange@hackermail.com",
+    "token" : "951df38b157c9f1bcbd450f3fd3d6840"
+  }
+
+$done({body: JSON.stringify(obj)});
